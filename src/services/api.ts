32f8d499274de692215c578af558c9b4b194d6b8
phdiamantino <51421386+phdiamantino@users.noreply.github.com>
2026-02-14 16:36:53 -0300

    fix: remove incorrect cors header from axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,9 @@ import axios from 'axios';
 
 export const backendApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-
-fetch("https://vispot-back.onrender.com/")
-  .then(r => r.json())
-  .then(console.log)
